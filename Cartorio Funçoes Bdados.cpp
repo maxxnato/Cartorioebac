68a98949d8c2db50c55f8915b010e94fecab1ea4
maxxnato <140678067+maxxnato@users.noreply.github.com>
2023-07-27 00:19:04 -0300

    Incluso no sistema a opção de saída para o usuário

--- Cartorio Funçoes Bdados.cpp
+++ Cartorio Funçoes Bdados.cpp
@@ -128,7 +128,8 @@ int main()
 		printf("Escolha a opção desejada do menu:\n\n");
 		printf("t1 - Registrar nomes\n");
 		printf("t2 - Consultar nomes\n");
-		printf("t3 - Deletar nomes\n\n");	
+		printf("t3 - Deletar nomes\n\n");
+		printf("t4 - Sair do sistema\n\n");	
 		printf("Opcão:"); //fim do menu
 	
 	
@@ -148,6 +149,11 @@ int main()
 			
 			case 3:
 			deletar();	//chamada da função deletar		
+			break;
+			
+			case 4:
+			printf("Obrigado por utilizar o sistema!\n");
+			return 0;
 			break;
 			
 			defalt:
